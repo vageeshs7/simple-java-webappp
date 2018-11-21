91c207088573da67bf4a7ea077deaf1f4c79a369
Shadakshari, Vageesh <Vageesh.Shadakshari@sc.com>
2018-11-21 16:26:07 +0530

    added undeploy before deploying

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,5 +3,8 @@ pwd
 echo 'List Existing Applications'
 curl -u both:tomcat http://localhost:8080/manager/text/list
 
+echo 'UnDeploy Applications'
+curl -u both:tomcat http://localhost:8080/manager/text/undeploy?path=/simple-java-webappp&tag=simple-java-webappp
+
 echo 'Deploy Applications'
 curl -T ./target/simple-java-webappp.war -u both:tomcat http://localhost:8080/manager/text/deploy?path=/simple-java-webappp&tag=simple-java-webappp
